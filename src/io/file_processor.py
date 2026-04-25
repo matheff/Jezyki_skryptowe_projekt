550c76cbb9e6c984c8914b0f5b4d77d4394b855a
matheff <matheff.00@gmail.com>
2026-04-25 19:22:57 +0200

    Fixed correct order in file_processor.py

--- src/io/file_processor.py
+++ src/io/file_processor.py
@@ -4,6 +4,7 @@ from src.models.sale_record import SaleRecord
 from src.models.product import Product
 import datetime
 
+
 class FileProcessor:
     def parse_sdf(self, path):
         sdf_file_path = Path(path)
@@ -21,94 +22,97 @@ class FileProcessor:
 
         try:
             with open(path, "r", encoding="utf-8") as f:
+                expected_order = ["DATASET", "PRODUCTS", "TRANSACTIONS"]
+                index = 0
                 for line in f:
                     line_number += 1
                     line = line.strip()
 
                     if line:
-
-                        if line == "---":
+                        if (line == "---"):
                             continue
 
-                        if (line.startswith('#')):
-                            if (line == "#DATASET"):
-                                section = "DATASET"
-                            elif (line == "#PRODUCTS"):
-                                section = "PRODUCTS"
-                            elif (line == "#TRANSACTIONS"):
-                                section = "TRANSACTIONS"
-                        else:
-                            if (section == "DATASET"):
-                                key, value = line.split(":", 1)
-                                if (key in ["owner", "index", "created", "currency"]):
-                                    dataset[key.strip()] = value.strip()
-                                else:
-                                    raise SdfParseError(f"Linia {line_number}: Błąd w sekcji #DATASET")
+                        if line.startswith("#"):
+                            new_section = line[1:]
 
-                            elif (section == "PRODUCTS"):
-                                parts = line.split("|")
-                                if len(parts) != 4:
-                                    raise SdfParseError(f"Linia {line_number}: Błąd w skecji #PRODUCTS")
-                                
-                                product_id = parts[0].strip()
-                                name = parts[1].strip()
-                                category = parts[2].strip()
-                                price = float(parts[3])
+                            if new_section != expected_order[index]:
+                                raise SdfParseError(f"Linia {line_number}: oczekiwano {expected_order[index]}, a jest {new_section}")
 
-                                products[product_id] = Product(
-                                                              product_id, 
-                                                              name, 
-                                                              category, 
-                                                              price
-                                                              )
-                                
-                            elif (section == "TRANSACTIONS"):
-                                parts = line.split("|")
+                            section = new_section
+                            index += 1
+                            continue
+                        
+                        if (section == "DATASET"):
+                            key, value = line.split(":", 1)
+                            if (key in ["owner", "index", "created", "currency"]):
+                                dataset[key.strip()] = value.strip()
+                            else:
+                                raise SdfParseError(f"Linia {line_number}: Błąd w sekcji #DATASET")
 
-                                if len(parts) != 5:
-                                    errors.append(f"Linia {line_number}: brakujące pole")
-                                    continue
+                        elif (section == "PRODUCTS"):
+                            parts = line.split("|")
+                            if len(parts) != 4:
+                                raise SdfParseError(f"Linia {line_number}: Błąd w skecji #PRODUCTS")
+                            
+                            product_id = parts[0].strip()
+                            name = parts[1].strip()
+                            category = parts[2].strip()
+                            price = float(parts[3])
 
-                                date, product_id, quantity, seller, region_code = parts
+                            products[product_id] = Product(
+                                                            product_id, 
+                                                            name, 
+                                                            category, 
+                                                            price
+                                                            )
+                            
+                        elif (section == "TRANSACTIONS"):
+                            parts = line.split("|")
 
-                                product_id = product_id.strip()
+                            if len(parts) != 5:
+                                errors.append(f"Linia {line_number}: brakujące pole")
+                                continue
 
-                                try:
-                                    quantity = int(quantity)
-                                except ValueError:
-                                    errors.append(f"Linia {line_number}: zły typ danych")
-                                    continue
+                            date, product_id, quantity, seller, region_code = parts
 
-                                if (region_code not in ["WA", "KR", "GD", "PO", "WR", 
-                                                        "LO", "RZ", "BY", "ZG", "OP"]):
-                                    errors.append(f"Linia {line_number}: nieprawidłowy kod regionu")
-                                    continue
+                            product_id = product_id.strip()
 
-                                if (product_id not in products):
-                                    errors.append(f"Linia {line_number}: nieistniejący product_id")
-                                    continue
+                            try:
+                                quantity = int(quantity)
+                            except ValueError:
+                                errors.append(f"Linia {line_number}: zły typ danych")
+                                continue
 
-                                product = products[product_id]
+                            if (region_code not in ["WA", "KR", "GD", "PO", "WR", 
+                                                    "LO", "RZ", "BY", "ZG", "OP"]):
+                                errors.append(f"Linia {line_number}: nieprawidłowy kod regionu")
+                                continue
 
-                                try:
-                                    date = datetime.datetime.strptime(date.strip(), "%d.%m.%Y").date()
-                                except:
-                                    errors.append(f"Linia {line_number}: niezgodny format daty")
-                                    continue
+                            if (product_id not in products):
+                                errors.append(f"Linia {line_number}: nieistniejący product_id")
+                                continue
 
-                                try:
-                                    record = SaleRecord(
-                                                        product,
-                                                        quantity,
-                                                        date,
-                                                        seller,
-                                                        region_code
-                                                        )
-                                except ValueError as e:
-                                    errors.append(f"Linia {line_number}: {e}")
-                                    continue
+                            product = products[product_id]
 
-                                transactions.append(record)
+                            try:
+                                date = datetime.datetime.strptime(date.strip(), "%d.%m.%Y").date()
+                            except:
+                                errors.append(f"Linia {line_number}: niezgodny format daty")
+                                continue
+
+                            try:
+                                record = SaleRecord(
+                                                    product,
+                                                    quantity,
+                                                    date,
+                                                    seller,
+                                                    region_code
+                                                    )
+                            except ValueError as e:
+                                errors.append(f"Linia {line_number}: {e}")
+                                continue
+
+                            transactions.append(record)
         except PermissionError:    
             raise PermissionError("Permission Error: plik jest nie do oczytu")
 
